0681234ea83dbbd5d29946d894e123723ca8384e
Puspak29 <samantapuspak@gmail.com>
2024-06-20 17:09:02 +0530

    added remove blank lines function

--- src/components/textForm.jsx
+++ src/components/textForm.jsx
@@ -5,11 +5,20 @@ import React, { useState, useRef } from 'react'
 
 function TextForm(props) {
   const [inputValue, setInputValue] = useState('Enter Your Text');
-  const copyRef= useRef(null)
+  // const [copiedText, setCopiedText] = useState('');
+  const copyRef = useRef(null)
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
+
+  // const pasteFromClip = async () => {
+  //   const text = await navigator.clipboard.readText()
+  //   console.log(text);
+  //   setCopiedText(text);
+  // }
+
+
   const formatText = (type) => {
     setInputValue((prev) => {
       if (type === "lower")
@@ -20,15 +29,20 @@ function TextForm(props) {
         return ""
       else if (type === "exSpaces")
         return prev.trim().replace(/\s+/g, ' ')
-      else if (type==="copy"){
+      else if (type === "copy") {
         copyRef.current?.select()
         window.navigator.clipboard.writeText(inputValue)
       }
+      else if (type === 'exLines') {
+        return prev.replace(/^\s*\n/gm, "");
+        // return prev + copiedText;
+      }
+
     })
   }
   return (
     <div>
-      <div className={`d-flex flex-column my-4 align-items-center`} style={{color: props.text}}>
+      <div className={`d-flex flex-column my-4 align-items-center`} style={{ color: props.text }}>
         <label htmlFor="exampleFormControlTextarea1" className="form-label">Enter Your Text Here</label>
         <textarea className={`w-75 form-control mb-3 border-primary`}
           type="text"
@@ -36,7 +50,7 @@ function TextForm(props) {
           value={inputValue}
           onChange={handleChange}
           rows={15}
-          style={{backgroundColor: props.mode==='light'?'black':'white', color: props.text}}
+          style={{ backgroundColor: props.mode === 'light' ? 'black' : 'white', color: props.text }}
           ref={copyRef}
         />
         <div className='flex-wrap align-items-center w-70'>
@@ -44,6 +58,7 @@ function TextForm(props) {
           <button className='btn btn-primary me-2 mb-2' onClick={() => { formatText("lower") }}>Lowercase</button>
           <button className='btn btn-primary me-2 mb-2' onClick={() => { formatText("clear") }}>Clear text</button>
           <button className='btn btn-primary me-2 mb-2' onClick={() => { formatText("copy") }}>Copy Text</button>
+          <button className='btn btn-primary me-2 mb-2' onClick={() => { formatText("exLines") }}>Remove Blank Lines</button>
           <button className='btn btn-primary me-2 mb-2' onClick={() => { formatText("exSpaces") }}>Remove Extra Spaces</button>
         </div>
       </div>
